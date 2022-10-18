d5a6eec36c7147f29bf695bfca0ba3203e2aa84f
mvazramos <vaz.ramosmiguel@gmail.com>
2022-10-18 12:40:18 +0100

    first refactor

--- app.js
+++ app.js
@@ -1,6 +1,45 @@
-
 /*  Array with the different legal plays for the game */
 const choices = ['Rock', 'Paper', 'Scissors'];
+
+// paths for images to display computer plays
+const computerImageSources={
+    "rock":"./images/stone.png",
+    "scissors":"./images/scissor.png",
+    "paper":"./images/paper.png"
+}
+
+
+/*
+    Object with all possible scenarios, except a tie;
+*/
+const scenarios = {
+    'rock':{
+        'paper':[-1, 'You lose! Paper beats Rock!','paper'],
+        'scissors': [1, 'You win! Rock beats Scissors!','scissors']
+    },
+    'paper':{
+        'rock':[1, 'You win! Paper beats Rock!','rock'],
+        'scissors':[-1, 'You lose! Scissors beats Paper!','scissors']
+    },
+    'scissors':{
+        'rock':[-1, 'You lose! Rock beats Scissors!','rock'],
+        'paper':[1, 'You win! Scissors beats Paper!','paper']
+    }
+}
+
+// game scores
+let computerScore = 0;
+let playerScore = 0;
+
+// gets all buttons defined in the game
+const buttons = document.querySelectorAll('button');
+
+
+//  .forEach will iterate through each button
+buttons.forEach((button) => {
+    // and for each one will add an eventListener
+    button.addEventListener('click', onClick);
+});
 
 
 function getComputerChoice() {
@@ -15,84 +54,17 @@ function playRound(playerSelection, computerSelection) {
     const player = playerSelection.toLowerCase();
     const computer = computerSelection.toLowerCase();
 
-    console.log(player, computer);
+    //console.log(player, computer);
 
     if (player === computer) {
 
         //return 'Its a Tie!!!';
-        return [0, 'Its a Tie!!!'];
+        return [0, 'Its a Tie!!!',computer];
 
-    } else if (player === 'rock' && computer === 'paper') {
-
-        //return 'You lose! Paper beats Rock';
-        return [-1, 'You lose! Paper beats Rock'];
-
-    } else if (player === 'paper' && computer === 'scissors') {
-
-        //return 'You lose! Scissors beats Paper';
-        return [-1, 'You lose! Scissors beats Paper'];
-
-    } else if (player === 'scissors' && computer === 'rock') {
-
-        //return 'You lose! Rock beats Scissors';
-        return [-1, 'You lose! Rock beats Scissors'];
-
-    } else if (player === 'paper' && computer === 'rock') {
-
-        //return 'You win! Paper beats Rock';
-        return [1, 'You win! Paper beats Rock'];
-
-    } else if (player === 'scissors' && computer === 'paper') {
-
-        //return 'You win! Scissors beats Paper';
-        return [1, 'You win! Scissors beats Paper'];
-
-    } else if (player === 'rock' && computer === 'scissors') {
-
-        //return 'You win! Rock beats Scissors';
-        return [1, 'You win! Rock beats Scissors'];
-
+    } else{
+        return scenarios[player][computer];
     }
 
-}
-
-// Test:
-
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice();
-// console.log(playRound(playerSelection, computerSelection));
-
-
-function game() {
-
-    let playerScore = 0;
-    let computerScore = 0;
-
-    for (let i = 0; i < 5; i++) {
-        let play = prompt("Enter - Rock, Paper or Scissors:")
-        let playResult = playRound(play, getComputerChoice())
-        //console.log(playResult);
-
-        if (playResult === 1) {
-            playerScore += 1;
-        } else if (playResult === -1) {
-            computerScore += 1;
-        }
-    }
-
-    if (playerScore === computerScore) {
-
-        return "It's a tie!";
-
-    } else if (playerScore > computerScore) {
-
-        return "You're the winner!";
-
-    } else {
-
-        return "You lost";
-
-    }
 }
 
 
@@ -101,6 +73,8 @@ function onClick() {
     let play = this.className;
     let computerPlay = getComputerChoice();
     let result = playRound(play, computerPlay);
+
+    const computerPlayImage = document.getElementsByClassName('computer-play');
 
     if (result[0] === 0) {
         document.getElementsByClassName('result-message')[0].innerHTML = result[1];
@@ -113,20 +87,12 @@ function onClick() {
         document.getElementsByClassName('result-message')[0].innerHTML = result[1];
         document.getElementById("computer-score").innerHTML = computerScore;
     }
-    console.log(result);
+    computerPlayImage[0].src=computerImageSources[result[2]];
+
 };
 
 
 
-// game scores
-let computerScore = 0;
-let playerScore = 0;
 
-// gets all buttons defined in the game
-const buttons = document.querySelectorAll('button');
 
-//  .forEach will iterate through each button
-buttons.forEach((button) => {
-    // and for each one will add an eventListener
-    button.addEventListener('click', onClick);
-});
+
